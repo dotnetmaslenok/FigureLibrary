b25e188ce8d6a52aa8d7a76bd8038f31110abd70
dotnetmaslenok <a.kudryavcev8080@gmail.com>
2022-11-15 19:20:33 +0300

    init

---
+++ FigureLibrary.Tests/BasicsScenarioTests.cs
@@ -0,0 +1,47 @@
+﻿namespace FigureLibrary.Tests;
+
+public sealed class BasicsScenarioTests
+{
+    [Theory]
+    [InlineData(3, 4, 5)]
+    public void CalculateTriangleArea_ShouldReturnValue_WhenItExists(double a, double b, double c)
+    {
+        Assert.Equal(6, AreaCalculator.Calculate(new Triangle(a ,b ,c)));
+    }
+
+    [Theory]
+    [InlineData(3, 5, 20)]
+    [InlineData(5, 15, 22)]
+    [InlineData(413, 123, 2)]
+    public void CalculateTriangleArea_ShouldThrowException_WhenItNotExists(double a, double b, double c)
+    {
+        Assert.Throws<ArgumentException>(() => AreaCalculator.Calculate(new Triangle(a, b, c)));
+    }
+
+    [Theory]
+    [InlineData(3, 4, 5)]
+    [InlineData(15, 20, 25)]
+    [InlineData(11, 60, 61)]
+    public void IsTriangleRight_ShouldReturnTrue_WhenArgsValid(double a, double b, double c)
+    {
+        var triangle = new Triangle(a, b, c);
+        Assert.True(triangle.IsRight());
+    }
+
+    [Theory]
+    [InlineData(3, 4, 6)]
+    [InlineData(15, 21, 25)]
+    [InlineData(44, 60, 75)]
+    public void IsTriangleRight_ShouldReturnFalse_WhenArgsInvalid(double a, double b, double c)
+    {
+        var triangle = new Triangle(a, b, c);
+        Assert.False(triangle.IsRight());
+    }
+
+    [Theory]
+    [InlineData(6)]
+    public void CalculateCircleArea_ShouldReturnValue_WhenRadiusValid(double radius)
+    {
+        Assert.Equal(113, Convert.ToInt64(AreaCalculator.Calculate(new Circle(radius))));
+    }
+}
---
+++ FigureLibrary/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FigureLibrary;
+
+public sealed class Triangle : IFigure
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+    
+    public Triangle([Range(0, double.MaxValue)] double a, [Range(0, double.MaxValue)] double b, [Range(0, double.MaxValue)] double c)
+    {
+        if (a + b <= c || a + c <= b || b + c <= a)
+            throw new ArgumentException("Sum of two any sides must be greater then 3rd side value");
+        
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public bool IsRight()
+    {
+        return Math.Pow(A, 2) + Math.Pow(B, 2) == Math.Pow(C, 2);;
+    }
+
+    public double Area()
+    {
+        var semiperimeter = (A + B + C) / 2;
+        return Math.Sqrt(semiperimeter * (semiperimeter - A) * (semiperimeter - B) * (semiperimeter - C));
+    }
+}
